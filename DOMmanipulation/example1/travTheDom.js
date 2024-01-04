094bee920427d7651c21119d1b4fd6f0cbeeb978
JC <j.c.ashley4363@gmail.com>
2024-01-04 08:18:26 -0600

    example 1 finished

---
+++ DOMmanipulation/example1/travTheDom.js
@@ -0,0 +1,25 @@
+// DOM Manipulation
+
+// Traverse the DOM
+
+// Parent Node Traversal
+
+// let ul = document.querySelector('ul')
+
+// console.log(ul.parentNode)
+// console.log(ul.parentElement)
+// console.log(ul.parentNode.parentNode)
+// console.log(ul.parentElement.parentElement)
+
+
+// Child Node Traversal
+// let ul = document.querySelector('ul')
+// console.log(ul.childNodes)
+// console.log(ul.firstChild)
+// console.log(ul.lastChild)
+
+// Sibiling Node Traversal
+
+// let ul = document.querySelector('ul')
+
+// console.log(ul.nextElementSibling)
