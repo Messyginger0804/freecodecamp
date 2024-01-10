4225e05d4c2e163564c8a780d71ce2acd013887a
JC <j.c.ashley4363@gmail.com>
2024-01-10 09:50:48 -0600

    dragonRepeller completed step 22

--- javaScriptAlgorithmsDataStructures/01dragonRepeller/script.js
+++ javaScriptAlgorithmsDataStructures/01dragonRepeller/script.js
@@ -1,6 +1,8 @@
 // Step 22
 // Initialize another variable called health with a value of 100, and a variable called gold with a value of 50.
+var health = 100;
 
-var xp;
+var gold = 50;
 
 var xp = 0;
+
