0aac1542c2bb0079c945baa0eb224c86c6bac394
JC <j.c.ashley4363@gmail.com>
2024-01-10 10:27:55 -0600

    dragonRepeller completed step step29

--- javaScriptAlgorithmsDataStructures/01dragonRepeller/script.js
+++ javaScriptAlgorithmsDataStructures/01dragonRepeller/script.js
@@ -1,11 +1,6 @@
 /*/ 
-Step 28
-// The player's inventory in your game will be able to hold multiple items. You will need to use a data type that can do this. An array can be used to hold multiple values. For example:
-
-// let order = ["first", "second", "third"];
-// This is an array which holds three values. Notice how the values are separated by commas.
-
-// Change your inventory variable to be an array with the strings stick, dagger, and sword.
+Step 29
+For now, you want the player to start with just the stick. Change the inventory array to have stick as its only value.
 */
 
 let xp = 0;
@@ -15,3 +10,4 @@ let currentWeapon = 0;
 let fighting;
 let monsterHealth;
 let inventory = ["stick", 'dagger', "sword"];
+// let inventory = ["stick", 'dagger', "sword"];
