edfebb6d0fb0294b4456460b5efc8c52f86cea41
JC <j.c.ashley4363@gmail.com>
2024-01-10 10:38:44 -0600

    dragonRepeller completed step step 33

--- javaScriptAlgorithmsDataStructures/01dragonRepeller/script.js
+++ javaScriptAlgorithmsDataStructures/01dragonRepeller/script.js
@@ -1,8 +1,6 @@
 /*/ 
-Step 32
-button1 is a variable that is not going to be reassigned. If you are not going to assign a new value to a variable, it is best practice to use the const keyword to declare it instead of the let keyword. This will tell JavaScript to throw an error if you accidentally reassign it.
-
-Change your button1 variable to be declared with the const keyword.
+Step 33
+Use querySelector() to get the other two button elements using their ids: button2 and button3. Store them in variables called button2 and button3. Remember to use const.
 */
 
 let xp = 0;
@@ -15,3 +13,5 @@ let inventory = ["stick"];
 // let inventory = ["stick", 'dagger', "sword"];
 
 const button1 = document.querySelector("#button1");
+const button2 = document.querySelector("#button2");
+const button3 = document.querySelector("#button3");
