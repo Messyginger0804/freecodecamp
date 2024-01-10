b109b39eda23838616f605a3d984a6db5062b56b
JC <j.c.ashley4363@gmail.com>
2024-01-10 15:11:24 -0600

    dragonRepeller completed step step 37

--- javaScriptAlgorithmsDataStructures/01dragonRepeller/script.js
+++ javaScriptAlgorithmsDataStructures/01dragonRepeller/script.js
@@ -12,10 +12,15 @@ const button1 = document.querySelector("#button1"); // initialize buttons
 const button2 = document.querySelector("#button2");
 const button3 = document.querySelector("#button3");
 
-// hello world
 
 /*
-  hello world
+ Step 37
+button1 represents your first button element. These elements have a special property called onclick, which you can use to determine what happens when someone clicks that button.
+
+You can access properties in JavaScript a couple of different ways. The first is with dot notation. Accessing the onclick property of a button would look like:
+
+button.onclick
+Use dot notation to set the onclick property of your button1 to the function reference of goStore. This function will be something you write later. Note that button1 is already declared, so you don't need to use let or const.
 */
 
 const text = document.querySelector('#text');
@@ -25,3 +30,6 @@ const goldText = document.querySelector('#goldText');
 const monsterStats = document.querySelector('#monsterStats');
 const monsterName = document.querySelector('#monsterName');
 const monsterHealthText = document.querySelector('#monsterHealth');
+
+
+button1.onclick = goStore();
