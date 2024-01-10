81b69d6e90c1849f316c1dbfaa11af823d616318
JC <j.c.ashley4363@gmail.com>
2024-01-10 15:19:27 -0600

    dragonRepeller completed step step 39

--- javaScriptAlgorithmsDataStructures/01dragonRepeller/script.js
+++ javaScriptAlgorithmsDataStructures/01dragonRepeller/script.js
@@ -1,4 +1,4 @@
-
+// declaring variables
 let xp = 0;
 let health = 100;
 let gold = 50;
@@ -6,32 +6,25 @@ let currentWeapon = 0;
 let fighting;
 let monsterHealth;
 let inventory = ["stick"];
-// let inventory = ["stick", 'dagger', "sword"];
 
-const button1 = document.querySelector("#button1"); // initialize buttons
+
+// declaring DOM variables
+const button1 = document.querySelector('#button1');
 const button2 = document.querySelector("#button2");
 const button3 = document.querySelector("#button3");
-
-
-/*
- Step 37
-button1 represents your first button element. These elements have a special property called onclick, which you can use to determine what happens when someone clicks that button.
-
-You can access properties in JavaScript a couple of different ways. The first is with dot notation. Accessing the onclick property of a button would look like:
-
-button.onclick
-Use dot notation to set the onclick property of your button1 to the function reference of goStore. This function will be something you write later. Note that button1 is already declared, so you don't need to use let or const.
-*/
-
-const text = document.querySelector('#text');
-const xpText = document.querySelector('#xpText');
-const healthText = document.querySelector('#healthText');
-const goldText = document.querySelector('#goldText');
-const monsterStats = document.querySelector('#monsterStats');
-const monsterName = document.querySelector('#monsterName');
-const monsterHealthText = document.querySelector('#monsterHealth');
+const text = document.querySelector("#text");
+const xpText = document.querySelector("#xpText");
+const healthText = document.querySelector("#healthText");
+const goldText = document.querySelector("#goldText");
+const monsterStats = document.querySelector("#monsterStats");
+const monsterName = document.querySelector("#monsterName");
+const monsterHealthText = document.querySelector("#monsterHealth");
 
 // initialize buttons
 button1.onclick = goStore;
 button2.onclick = goCave;
 button3.onclick = fightDragon;
+
+function goStore() {
+
+}
