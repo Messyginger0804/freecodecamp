e7035525918326c6257059eb8a0e528630dc9cbf
JC <j.c.ashley4363@gmail.com>
2024-01-10 10:32:01 -0600

    dragonRepeller completed step step 30

--- javaScriptAlgorithmsDataStructures/01dragonRepeller/script.js
+++ javaScriptAlgorithmsDataStructures/01dragonRepeller/script.js
@@ -1,6 +1,13 @@
 /*/ 
-Step 29
-For now, you want the player to start with just the stick. Change the inventory array to have stick as its only value.
+Step 30
+JavaScript interacts with the HTML using the Document Object Model, or DOM. The DOM is a tree of objects that represents the HTML. You can access the HTML using the document object, which represents your entire HTML document.
+
+One method for finding specific elements in your HTML is using the querySelector() method. The querySelector() method takes a CSS selector as an argument and returns the first element that matches that selector. For example, to find the <h1> element in your HTML, you would write:
+
+let h1 = document.querySelector("h1");
+Note that h1 is a string and matches the CSS selector you would use.
+
+Create a button1 variable and use querySelector() to assign it your element with the id of button1. Remember that CSS id selectors are prefixed with a #.
 */
 
 let xp = 0;
@@ -9,5 +16,7 @@ let gold = 50;
 let currentWeapon = 0;
 let fighting;
 let monsterHealth;
-let inventory = ["stick", 'dagger', "sword"];
+let inventory = ["stick"];
 // let inventory = ["stick", 'dagger', "sword"];
+
+let button1 = document.querySelector("#button1");
