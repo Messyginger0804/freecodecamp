b3d7f4e185921cb5f2cb0bb03fb054995a36ce36
JC <j.c.ashley4363@gmail.com>
2024-01-10 10:04:22 -0600

    dragonRepeller completed step 27

--- javaScriptAlgorithmsDataStructures/01dragonRepeller/script.js
+++ javaScriptAlgorithmsDataStructures/01dragonRepeller/script.js
@@ -1,17 +1,7 @@
-/*
-Step 26
-Declare two more variables named monsterHealth and inventory, but do not initialize them.
-*/
-let health = 100;
-
-let gold = 50;
-
 let xp = 0;
-
+let health = 100;
+let gold = 50;
 let currentWeapon = 0;
-
 let fighting;
-
 let monsterHealth;
-
-let inventory;
+let inventory = "stick";
