a07f4573787432c0d0168427d81be2e5251798ca
JC <j.c.ashley4363@gmail.com>
2024-01-10 09:55:01 -0600

    dragonRepeller completed step 23

--- javaScriptAlgorithmsDataStructures/01dragonRepeller/script.js
+++ javaScriptAlgorithmsDataStructures/01dragonRepeller/script.js
@@ -1,8 +1,14 @@
-// Step 22
-// Initialize another variable called health with a value of 100, and a variable called gold with a value of 50.
+/*
+// Step 23
+// Create another variable called currentWeapon and set it to 0.
+
+// When a variable name has multiple words, the convention in JavaScript is to use what's called camelCase. The first word is lowercase, and the first letter of every following word is uppercase.
+*/
 var health = 100;
 
 var gold = 50;
 
 var xp = 0;
+
+var currentWeapon = 0;
 
