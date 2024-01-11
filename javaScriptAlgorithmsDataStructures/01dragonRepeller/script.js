e56eddda282d28e4b48c79e4c9b72dc089640a61
JC <j.c.ashley4363@gmail.com>
2024-01-10 20:31:41 -0600

    dragonRepeller completed step step 45

--- javaScriptAlgorithmsDataStructures/01dragonRepeller/script.js
+++ javaScriptAlgorithmsDataStructures/01dragonRepeller/script.js
@@ -26,9 +26,15 @@ function goStore() {
     button1.innerText = "Buy 10 health (10 gold)";
     button2.innerText = "Buy weapon (30 gold)";
     button3.innerText = "Go to town square";
+
+    button1.onclick = buyHealth;
+    button2.onclick = buyWeapon;
+    button3.onclick = goTown;
 }
-// Step 44
-// Now, add a line that updates the text of button2 to say Buy weapon (30 gold) and update the text of button3 to say Go to town square.
+// Step 45
+// You will also need to update the functions that run when the buttons are clicked again.
+
+// In your goStore() function, update the onclick property for each button to run buyHealth, buyWeapon, and goTown, respectively.
 function goCave() {
     console.log("Going to cave.");
 }
