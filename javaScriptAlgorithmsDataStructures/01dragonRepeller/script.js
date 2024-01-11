1231bcebb4196ef28709956bc7944c00126bea2e
JC <j.c.ashley4363@gmail.com>
2024-01-11 09:54:14 -0600

    dragonRepeller completed step 58

--- javaScriptAlgorithmsDataStructures/01dragonRepeller/script.js
+++ javaScriptAlgorithmsDataStructures/01dragonRepeller/script.js
@@ -21,12 +21,13 @@ const locations = [
         name: "town square",
         "button text": ["Go to store", "Go to cave", "Fight dragon"],
         "button functions": [goStore, goCave, fightDragon],
+        text: "You are in the town square. You see a sign that says \"Store\"."
     },
 ];
 
 /*
-Step 57
-Create another property in your object called button functions. Give this property an array containing the three functions assigned to the onclick properties in the goTown function. Remember that these functions are variables, not strings, and should not be wrapped in quotes.
+Step 58
+Add one final property to the object named text. Give this property the same string value as the one assigned to text.innerText in the goTown function.
 */
 
 
