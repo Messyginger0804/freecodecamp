8f9d27a32022138d25c7b17671424f41a36e9589
JC <j.c.ashley4363@gmail.com>
2024-01-10 20:26:55 -0600

    dragonRepeller completed step step 44

--- javaScriptAlgorithmsDataStructures/01dragonRepeller/script.js
+++ javaScriptAlgorithmsDataStructures/01dragonRepeller/script.js
@@ -24,8 +24,11 @@ button3.onclick = fightDragon;
 
 function goStore() {
     button1.innerText = "Buy 10 health (10 gold)";
+    button2.innerText = "Buy weapon (30 gold)";
+    button3.innerText = "Go to town square";
 }
-
+// Step 44
+// Now, add a line that updates the text of button2 to say Buy weapon (30 gold) and update the text of button3 to say Go to town square.
 function goCave() {
     console.log("Going to cave.");
 }
