c43a8c61c079defc3ea6e2f3505da8505a551f59
JC <j.c.ashley4363@gmail.com>
2024-01-10 20:24:09 -0600

    dragonRepeller completed step step 42

--- javaScriptAlgorithmsDataStructures/01dragonRepeller/script.js
+++ javaScriptAlgorithmsDataStructures/01dragonRepeller/script.js
@@ -1,4 +1,3 @@
-// declaring variables
 let xp = 0;
 let health = 100;
 let gold = 50;
@@ -7,8 +6,6 @@ let fighting;
 let monsterHealth;
 let inventory = ["stick"];
 
-
-// declaring DOM variables
 const button1 = document.querySelector('#button1');
 const button2 = document.querySelector("#button2");
 const button3 = document.querySelector("#button3");
@@ -24,11 +21,24 @@ const monsterHealthText = document.querySelector("#monsterHealth");
 button1.onclick = goStore;
 button2.onclick = goCave;
 button3.onclick = fightDragon;
+/*
+The innerText property controls the text that appears in an HTML element. For example:
+
+const info = document.querySelector("#info");
+info.innerText = "Hello World";
+This code would change the element assigned to the info variable to have the text Hello World.
+
+When a player clicks your Go to store button, you want to change the buttons and text. Remove the code inside the goStore function and add a line that updates the text of button1 to say Buy 10 health (10 gold).
+*/
 
 function goStore() {
-    console.log('Going to store.');
+    button1.innerText = "Buy 10 health (10 gold)";
 }
 
 function goCave() {
-    console.log('Going to cave.');
+    console.log("Going to cave.");
+}
+
+function fightDragon() {
+    console.log("Fighting dragon.");
 }
