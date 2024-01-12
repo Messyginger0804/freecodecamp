383da6cf66e28e943052cdd95d967061c4100e7b
JC <j.c.ashley4363@gmail.com>
2024-01-12 10:11:58 -0600

    dragonRepeller completed step 74

--- javaScriptAlgorithmsDataStructures/01dragonRepeller/script.js
+++ javaScriptAlgorithmsDataStructures/01dragonRepeller/script.js
@@ -67,9 +67,11 @@ function goCave() {
 function fightDragon() {
     console.log("Fighting dragon.");
 }
-
+// Step 74
+// After the gold is updated, add a line to set health equal to health plus 10.
 function buyHealth() {
     gold = gold - 10;
+    health = health + 10;
 }
 
 function buyWeapon() {
