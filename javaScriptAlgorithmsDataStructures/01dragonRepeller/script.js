3aa44ff881fe65a173efeb264787bed1153a6147
JC <j.c.ashley4363@gmail.com>
2024-01-12 10:31:21 -0600

    dragonRepeller completed step 79

--- javaScriptAlgorithmsDataStructures/01dragonRepeller/script.js
+++ javaScriptAlgorithmsDataStructures/01dragonRepeller/script.js
@@ -68,12 +68,25 @@ function fightDragon() {
     console.log("Fighting dragon.");
 }
 
+// Step 79
+// Now when a player tries to buy health, it will only work if they have enough money. If they do not, nothing will happen. Add an else statement where you can put code to run if a player does not have enough money.
+
+// Here is an example of an empty else statement:
+
+// if (num >= 5) {
+
+// } else {
+
+// }
+
 function buyHealth() {
     if (gold >= 10) {
         gold -= 10;
         health += 10;
         goldText.innerText = gold;
         healthText.innerText = health;
+    } else {
+
     }
 }
 
