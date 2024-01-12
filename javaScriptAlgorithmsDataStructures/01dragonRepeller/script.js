ade0e426ddb304a052a62aa505dbd78572857977
JC <j.c.ashley4363@gmail.com>
2024-01-11 20:35:26 -0600

    dragonRepeller completed step 64

--- javaScriptAlgorithmsDataStructures/01dragonRepeller/script.js
+++ javaScriptAlgorithmsDataStructures/01dragonRepeller/script.js
@@ -30,23 +30,14 @@ const locations = [
         text: "You enter the store."
     }
 ];
-/*
-Step 63
-The locations array contains two locations: the town square and the store. Currently you are passing that entire array into the update function.
-
-Pass in only the first element of the locations array by adding [0] at the end of the variable. For example: myFunction(arg[0]);.
-
-This is called bracket notation. Values in an array are accessed by index. Indices are numerical values and start at 0 - this is called zero-based indexing. arg[0] would be the first element in the arg array.
-*/
 
 // initialize buttons
 button1.onclick = goStore;
 button2.onclick = goCave;
 button3.onclick = fightDragon;
 
-function update(locations) {
-
-    button1.innerText = "Go to store";
+function update(location) {
+    button1.innerText = location["button text"];
     button2.innerText = "Go to cave";
     button3.innerText = "Fight dragon";
     button1.onclick = goStore;
@@ -60,6 +51,7 @@ function goTown() {
 }
 
 function goStore() {
+
 }
 
 function goCave() {
