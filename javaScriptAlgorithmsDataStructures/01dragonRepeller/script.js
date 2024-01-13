2e03238d795f1a7ac60c09d491879cc7d45cffdc
JC <j.c.ashley4363@gmail.com>
2024-01-13 10:11:02 -0600

    dragonRepeller completed step 106

--- javaScriptAlgorithmsDataStructures/01dragonRepeller/script.js
+++ javaScriptAlgorithmsDataStructures/01dragonRepeller/script.js
@@ -105,6 +105,9 @@ function buyWeapon() {
     }
 }
 
+// Step 108
+// Use an else statement to run when the inventory length is not more than one. Set the text.innerText to say Don't sell your only weapon!.
+
 function sellWeapon() {
     if (inventory.length > 1) {
         gold += 15;
@@ -112,6 +115,8 @@ function sellWeapon() {
         let currentWeapon = inventory.shift();
         text.innerText = "You sold a " + currentWeapon + ". ";
         text.innerText += " In your inventory you have: " + inventory;
+    } else {
+        text.innerText = "Don't sell your only weapon!";
     }
 }
 
