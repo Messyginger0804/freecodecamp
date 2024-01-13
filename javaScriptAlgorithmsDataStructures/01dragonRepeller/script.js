f0e3f45ae8695898225c41cd81264c5f9f931637
JC <j.c.ashley4363@gmail.com>
2024-01-12 20:23:38 -0600

    dragonRepeller completed step 88

--- javaScriptAlgorithmsDataStructures/01dragonRepeller/script.js
+++ javaScriptAlgorithmsDataStructures/01dragonRepeller/script.js
@@ -84,13 +84,14 @@ function buyHealth() {
         text.innerText = "You do not have enough gold to buy health.";
     }
 }
-
+// Step 88
+// You should tell the player what weapon they bought. In between the two lines you just wrote, use let to initialize a new variable called newWeapon. Set this to equal weapons.
 function buyWeapon() {
     if (gold >= 30) {
         gold -= 30;
         currentWeapon++;
         goldText.innerText = gold;
-
+        let newWeapon = weapons;
         text.innerText = "You now have a new weapon.";
     }
 }
