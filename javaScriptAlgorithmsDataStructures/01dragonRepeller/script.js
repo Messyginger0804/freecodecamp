a22179a4b7aa6f0de1f4c19e1d9861c697bca5cd
JC <j.c.ashley4363@gmail.com>
2024-01-13 09:36:06 -0600

    dragonRepeller completed step 103

--- javaScriptAlgorithmsDataStructures/01dragonRepeller/script.js
+++ javaScriptAlgorithmsDataStructures/01dragonRepeller/script.js
@@ -105,11 +105,14 @@ function buyWeapon() {
     }
 }
 
-// Step 102
-// Players should not be able to sell their only weapon. Inside the sellWeapon function, add an if statement with a condition that checks if the length of the inventory array is greater than 1.
+// Step 103
+// Inside the if statement, set gold equal to 15 more than its current value. Also update goldText.innerText to the new value.
 
 function sellWeapon() {
-    if (inventory.length > 1) { }
+    if (inventory.length > 1) {
+        gold += 15
+        goldText.innerText = gold;
+    }
 
 }
 
