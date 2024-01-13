8b6948553ff40d2087ec3612587ba9859b48cec9
JC <j.c.ashley4363@gmail.com>
2024-01-13 10:18:06 -0600

    dragonRepeller completed step 111

--- javaScriptAlgorithmsDataStructures/01dragonRepeller/script.js
+++ javaScriptAlgorithmsDataStructures/01dragonRepeller/script.js
@@ -129,9 +129,14 @@ function sellWeapon() {
         text.innerText = "Don't sell your only weapon!";
     }
 }
+// Step 111
+// In your fightSlime function, set fighting equal to 0 - the index of slime in the monsters array. Remember that you already declared fighting earlier in your code, so you do not need let or const here.
+
+// On the next line, call the goFight function.
 
 function fightSlime() {
-
+    fighting = 0;
+    goFight();
 }
 
 function fightBeast() {
