141853ed79ba1dab6edf173717e63ce8a86a3d98
JC <j.c.ashley4363@gmail.com>
2024-01-12 20:40:57 -0600

    dragonRepeller completed step 93

--- javaScriptAlgorithmsDataStructures/01dragonRepeller/script.js
+++ javaScriptAlgorithmsDataStructures/01dragonRepeller/script.js
@@ -85,6 +85,9 @@ function buyHealth() {
     }
 }
 
+// Step 94
+// At the end of the second text.innerText string you just added, use the concatenation operator to add the contents of inventory to the string.
+
 function buyWeapon() {
     if (gold >= 30) {
         gold -= 30;
