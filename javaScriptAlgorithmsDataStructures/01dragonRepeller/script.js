75b464144a46896e3b37a8028ad67b8f17beb1d4
JC <j.c.ashley4363@gmail.com>
2024-01-18 17:57:21 -0600

    dragonRepeller completed step 134

--- javaScriptAlgorithmsDataStructures/01dragonRepeller/script.js
+++ javaScriptAlgorithmsDataStructures/01dragonRepeller/script.js
@@ -72,6 +72,11 @@ const locations = [
     }
 ];
 
+// Step 134
+// The word Arg! should have quotes around it. Besides escaping quotes, there is another way you can include quotation marks inside a string.
+
+// Change the double quotes around the string The monster screams Arg! as it dies. You gain experience points and find gold. to single quotes ', then add double quotes around Arg!.
+
 // initialize buttons
 button1.onclick = goStore;
 button2.onclick = goCave;
