7436afde2dc55063e09f7f28ecb11cb893bbbf98
JC <j.c.ashley4363@gmail.com>
2024-01-18 20:26:01 -0600

    dragonRepeller completed step 145

--- javaScriptAlgorithmsDataStructures/01dragonRepeller/script.js
+++ javaScriptAlgorithmsDataStructures/01dragonRepeller/script.js
@@ -196,7 +196,14 @@ function attack() {
     }
 }
 
-function getMonsterAttackValue(level) { }
+// Step 145
+// The attack of the monster will be based on the monster's level and the player's xp. In the getMonsterAttackValue function, use const to create a variable called hit. Assign it the equation (level * 5) - (Math.floor(Math.random() * xp));.
+
+// This will set the monster's attack to five times their level minus a random number between 0 and the player's xp.
+
+function getMonsterAttackValue(level) {
+    const hit = (level * 5) - (Math.floor(Math.random() * xp));
+}
 
 function dodge() {
     text.innerText = "You dodge the attack from the " + monsters[fighting].name;
