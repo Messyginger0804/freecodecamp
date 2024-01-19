9e805961f52c7f83208d33a516f2ed1e54d87424
JC <j.c.ashley4363@gmail.com>
2024-01-18 19:39:22 -0600

    dragonRepeller completed step 141

--- javaScriptAlgorithmsDataStructures/01dragonRepeller/script.js
+++ javaScriptAlgorithmsDataStructures/01dragonRepeller/script.js
@@ -202,7 +202,12 @@ function lose() {
     update(locations[5]);
 }
 
+function winGame() {
+    update(locations[6]);
+}
 
+// Step 141
+// After the lose function, create a function called winGame. Inside the winGame function, call the update function and pass in locations[6].
 
 function restart() {
     xp = 0;
