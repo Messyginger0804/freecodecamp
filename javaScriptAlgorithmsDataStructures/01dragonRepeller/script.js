089852679ad20498edb45210cd23b9b451f992c9
JC <j.c.ashley4363@gmail.com>
2024-01-18 20:56:04 -0600

    dragonRepeller completed step 155

--- javaScriptAlgorithmsDataStructures/01dragonRepeller/script.js
+++ javaScriptAlgorithmsDataStructures/01dragonRepeller/script.js
@@ -179,8 +179,6 @@ function goFight() {
     monsterHealthText.innerText = monsterHealth;
 }
 
-// Step 154
-// On every attack, there should be a chance that the player's weapon breaks. At the end of the attack function, add an empty if statement with the condition Math.random() <= .1.
 function attack() {
     text.innerText = "The " + monsters[fighting].name + " attacks.";
     text.innerText += " You attack it with your " + weapons[currentWeapon].name + ".";
@@ -198,8 +196,12 @@ function attack() {
     } else if (monsterHealth <= 0) {
         fighting === 2 ? winGame() : defeatMonster();
     }
-    if (Math.random() <= .1) { }
+    if (Math.random() <= .1) {
+        text.innerText += " Your " + inventory.pop() + " breaks."
+    }
 }
+// Step 155
+// Use the += operator to add Your <weapon> breaks., with a space in front of Your, to the end of text.innerText. Replace <weapon> with the last item in the inventory array using inventory.pop(), which will remove the last item in the array AND return it so it appears in your string.
 
 
 function getMonsterAttackValue(level) {
