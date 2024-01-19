f7cf3edca84ffdb2834084bc61c94accc11e3193
JC <j.c.ashley4363@gmail.com>
2024-01-18 21:42:25 -0600

    dragonRepeller completed step 171

--- javaScriptAlgorithmsDataStructures/01dragonRepeller/script.js
+++ javaScriptAlgorithmsDataStructures/01dragonRepeller/script.js
@@ -259,10 +259,8 @@ function pickTwo() {
 function pickEight() {
     pick(8);
 }
-// Step 170
-// The last statement in a for loop, the final expression, is executed at the end of each loop iteration.
-
-// Since we changed the initialization statement to use i instead of x, change x++ to i++. This will increment the initializer i by 1 after each loop.
+// Step 171
+// Now you can write the logic to run in the loop. Inside your for loop, use the += operator to add to the end of text.innerText. Add the number at index i of the numbers array, using numbers[i]. Then add a new line, using the escape sequence you used earlier.
 function pick(guess) {
     const numbers = [];
     while (numbers.length < 10) {
@@ -270,6 +268,6 @@ function pick(guess) {
     }
     text.innerText = "You picked " + guess + ". Here are the random numbers:\n";
     for (let i = 0; i < 10; i++) {
-
+        text.innerText += numbers[i] + "\n";
     }
 }
