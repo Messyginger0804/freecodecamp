ae3771dd4eef8d3ca5ec3bfa34a0eabdcfac0dd0
JC <j.c.ashley4363@gmail.com>
2024-01-19 21:16:15 -0600

    carlorieCounter completed step 21

--- javaScriptAlgorithmsDataStructures/02calorieCounter/script.js
+++ javaScriptAlgorithmsDataStructures/02calorieCounter/script.js
@@ -9,6 +9,22 @@ const clearButton = document.getElementById('clear');
 const output = document.getElementById('output');
 let isError = false;
 
+// Step 22
+// Within your loop, you need to check if the character in strArray at index i is not a +, -, or a space. If it is not, push it to the cleanStrArray.
+
+// You will need to check if the array ["+", "-", " "] does not include the current character. You can use a combination of the includes() method and the ! operator to do this.
+
+// The .includes() method returns true if the array contains the character, and false if not. The logical NOT operator (!) will return the opposite of the value of the .includes() method.
+
+// Here is an example:
+
+// const numbersArray = [1, 2, 3, 4, 5]
+// const number = 6
+
+// if (!numbersArray.includes(number)) {
+//   console.log("The number is not in the array.")
+// }
+
 function cleanInputString(str) {
     const strArray = str.split('');
     const cleanStrArray = [];
