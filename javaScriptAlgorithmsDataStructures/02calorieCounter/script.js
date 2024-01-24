4a54b47c03bcb7e2e46cbe897ee40fede89dd8f9
JC <j.c.ashley4363@gmail.com>
2024-01-24 13:19:05 -0600

    calorieCounter completed step 23

--- javaScriptAlgorithmsDataStructures/02calorieCounter/script.js
+++ javaScriptAlgorithmsDataStructures/02calorieCounter/script.js
@@ -6,13 +6,14 @@ const clearButton = document.getElementById('clear');
 const output = document.getElementById('output');
 let isError = false;
 
-function cleanInputString(str) {
-    const strArray = str.split('');
-    const cleanStrArray = [];
 
-    for (let i = 0; i < strArray.length; i++) {
-        if (!["+", "-", " "].includes(strArray[i])) {
-            cleanStrArray.push(strArray[i])
-        }
-    }
+// Step 23
+// While looping through the string works, creating a new array is inefficient for memory and runtime performance. Instead, you can use Regular Expressions (referred to as "regex") to match specific characters.
+
+// Regex in JavaScript is indicated by a pattern wrapped in forward slashes – for example:
+
+// const regex = /hello/;
+// Remove your existing code within the cleanInputString function. Declare a regex variable and assign it the value from the example above.
+function cleanInputString(str) {
+    const regex = /hello/;
 }
