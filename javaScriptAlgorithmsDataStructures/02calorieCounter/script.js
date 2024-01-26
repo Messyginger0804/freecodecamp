65f6065ea1087b6ae0eb90ae29b6c654c098e084
JC <j.c.ashley4363@gmail.com>
2024-01-26 10:23:50 -0600

    calorie counter completed step 29

--- javaScriptAlgorithmsDataStructures/02calorieCounter/script.js
+++ javaScriptAlgorithmsDataStructures/02calorieCounter/script.js
@@ -6,8 +6,14 @@ const clearButton = document.getElementById('clear');
 const output = document.getElementById('output');
 let isError = false;
 
+
+// Step 29
+// In HTML, number inputs allow for exponential notation (such as 1e10). You need to filter those out.
+
+// Start by creating a function called isInvalidInput – it should take a single str parameter.
 function cleanInputString(str) {
     const regex = /[+-\s]/g;
     return str.replace(regex, '');
 }
 
+function isInvalidInput(str) { }
