23e50f1fe2fd86bd3e38425f47e481f6ccced5bf
JC <j.c.ashley4363@gmail.com>
2024-01-26 10:48:26 -0600

    calorie counter completed step 40

--- javaScriptAlgorithmsDataStructures/02calorieCounter/script.js
+++ javaScriptAlgorithmsDataStructures/02calorieCounter/script.js
@@ -16,19 +16,9 @@ function isInvalidInput(str) {
     return str.match(regex);
 }
 
-// Step 39
-// JavaScript has a feature called template literals, which allow you to interpolate variables directly within a string. Template literals are denoted with backticks ``, as opposed to single or double quotes. Variables can be passed in to a template literal by surrounding the variable with ${} – the value of the variable will be inserted into the string.
-
-// For example:
-
-// const name = "Naomi";
-// const templateLiteral = `Hello, my name is ${name}~!`;
-// console.log(templateLiteral);
-// The console will show the string "Hello, my name is Naomi~!".
-
-// Replace your concatenated string in the querySelector with a template literal – be sure to keep the space between your targetId variable and .input-container.
+// Step 40
+// Thanks to template literals, you actually don't need the targetId variable at all. Remove that variable, and update your template literal to replace targetId with entryDropdown.value – remember to add # before that, in the string.
 
 function addEntry() {
-    const targetId = '#' + entryDropdown.value;
-    const targetInputContainer = document.querySelector(`${targetId} .input-container`);
+    const targetInputContainer = document.querySelector(`#${entryDropdown.value} .input-container`);
 }
