d19e7615a884dd9271b81f13fae1a7c4bc08653d
JC <j.c.ashley4363@gmail.com>
2024-01-27 09:39:34 -0600

    calorie counter completed step 46

--- javaScriptAlgorithmsDataStructures/02calorieCounter/script.js
+++ javaScriptAlgorithmsDataStructures/02calorieCounter/script.js
@@ -20,9 +20,7 @@ function addEntry() {
     const targetInputContainer = document.querySelector(`#${entryDropdown.value} .input-container`);
     const entryNumber = targetInputContainer.querySelectorAll('input[type="text"]').length;
     const HTMLString = `
-      <label for="${entryDropdown.value}-${entryNumber}-name">Entry ${entryNumber} Name</label>`;
+      <label for="${entryDropdown.value}-${entryNumber}-name">Entry ${entryNumber} Name</label>
+      <input type="text" placeholder="Name" id="${entryDropdown.value}-${entryNumber}-name">
+      `;
 }
-
-
-// Step 45
-// Give your label element a for attribute with the value X-#-name, where X is the value of the entryDropdown element and # is the value of entryNumber. Remember that HTML attributes should be wrapped in double quotes.
