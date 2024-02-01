1b9d2868c412fabcedd8b304bc1a74eb313d6931
JC <j.c.ashley4363@gmail.com>
2024-02-01 10:01:52 -0600

    calorieCounter completed step 53

--- javaScriptAlgorithmsDataStructures/02calorieCounter/script.js
+++ javaScriptAlgorithmsDataStructures/02calorieCounter/script.js
@@ -29,9 +29,14 @@ function addEntry() {
       id="${entryDropdown.value}-${entryNumber}-calories"
       placeholder="Calories"
     />`;
-    targetInputContainer.insertAdjacentHTML();
+    targetInputContainer.insertAdjacentHTML('beforeend', HTMLString);
 }
 
+// Step 53
+// The insertAdjacentHtml method takes two arguments. The first argument is a string that specifies the position of the inserted element. The second argument is a string containing the HTML to be inserted.
 
+// For the first argument, pass the string beforeend to insert the new element as the last child of targetInputContainer.
+
+// For the second argument, pass your HTMLString variable.
 
 addEntryButton.addEventListener("click", addEntry);
