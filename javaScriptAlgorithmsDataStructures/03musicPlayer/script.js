26e2f0408df6e2935c1b6be4616f182c6daae71c
JC <j.c.ashley4363@gmail.com>
2024-02-05 13:51:32 -0600

    musicPlayer completed step 16

--- javaScriptAlgorithmsDataStructures/03musicPlayer/script.js
+++ javaScriptAlgorithmsDataStructures/03musicPlayer/script.js
@@ -91,9 +91,12 @@ const renderSongs = (array) => {
         .map((song) => {
             return `
       <li id="song-${song.id}" class="playlist-song">
+      <button class="playlist-song-info">
+          <span class="playlist-song-title">${song.title}</span>
+          <span class="playlist-song-artist">${song.artist}</span>
+          <span class="playlist-song-duration">${song.duration}</span>
 
-      
-      <button class='playlist-song-info'><span class='playlist-song-title'>${song.title}</span></button>
+      </button>
       </li>
       `;
         })
