6ac170d323cbd1396b0fc0866b88df9cd5f55175
JC <j.c.ashley4363@gmail.com>
2024-02-07 09:53:45 -0600

    musicPlayer compeleted step 44

--- javaScriptAlgorithmsDataStructures/03musicPlayer/script.js
+++ javaScriptAlgorithmsDataStructures/03musicPlayer/script.js
@@ -108,6 +108,9 @@ const pauseSong = () => {
     audio.pause();
 };
 
+// Step 45
+// Add an else block to the if statement. Inside the else block, call the getCurrentSongIndex() function and assign it to a constant named currentSongIndex.
+
 const playNextSong = () => {
     if (userData?.currentSong === null) {
         playSong(userData?.songs[0].id);
