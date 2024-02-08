ad10dc7d5d93f6e4a33168e22967ff7f6ad4edca
JC <j.c.ashley4363@gmail.com>
2024-02-08 16:16:38 -0600

    musicPlayer compeleted step 65

--- javaScriptAlgorithmsDataStructures/03musicPlayer/script.js
+++ javaScriptAlgorithmsDataStructures/03musicPlayer/script.js
@@ -100,6 +100,7 @@ const playSong = (id) => {
 
     highlightCurrentSong();
     setPlayerDisplay();
+
     audio.play();
 };
 
@@ -179,6 +180,10 @@ const renderSongs = (array) => {
 const setPlayButtonAccessibleText = () => {
     const song = userData?.currentSong || userData?.songs[0];
 
+    playButton.setAttribute(
+        "aria-label",
+        song?.title ? `Play ${song.title}` : "Play"
+    );
 };
 
 const getCurrentSongIndex = () => userData?.songs.indexOf(userData?.currentSong);
