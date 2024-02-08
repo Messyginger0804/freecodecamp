077d74b8ee83b10527e9c7ef66cd213d272bd56f
JC <j.c.ashley4363@gmail.com>
2024-02-08 09:18:42 -0600

    musicPlayer compeleted step 55

--- javaScriptAlgorithmsDataStructures/03musicPlayer/script.js
+++ javaScriptAlgorithmsDataStructures/03musicPlayer/script.js
@@ -129,26 +129,15 @@ const playPreviousSong = () => {
     }
 };
 
-// Step 54
-// Loop through the playlistSongElements with a forEach method.
-
-// The forEach method is used to loop through an array and perform a function on each element of the array. For example, suppose you have an array of numbers and you want to log each number to the console.
-
-// const numbers = [1, 2, 3, 4, 5];
-
-// // Using forEach to iterate through the array
-// numbers.forEach((number) => {
-//   console.log(number); // 1, 2, 3, 4, 5
-// });
-// Use the forEach method on playlistSongElements. Pass in songEl as the parameter and use arrow syntax to add in an empty callback.
-
 const highlightCurrentSong = () => {
     const playlistSongElements = document.querySelectorAll(".playlist-song");
     const songToHighlight = document.getElementById(
         `song-${userData?.currentSong?.id}`
     );
 
-    playlistSongElements.forEach((songEl) => { })
+    playlistSongElements.forEach((songEl) => {
+        songEl.removeAttribute("aria-current")
+    });
 };
 
 const renderSongs = (array) => {
