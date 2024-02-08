faa5cc4f698f993b0a9ac7864b761a5c586c6f08
JC <j.c.ashley4363@gmail.com>
2024-02-08 07:19:33 -0600

    musicPlayer compeleted step 53

--- javaScriptAlgorithmsDataStructures/03musicPlayer/script.js
+++ javaScriptAlgorithmsDataStructures/03musicPlayer/script.js
@@ -129,14 +129,14 @@ const playPreviousSong = () => {
     }
 };
 
-// Step 52
-// If you check closely, you'd see the currently playing song is not highlighted in the playlist, so you don't really know which song is playing. You can fix this by creating a function to highlight any song that is being played.
-
-// Using an arrow syntax, create a highlightCurrentSong function. Inside the function, use querySelectorAll to get the .playlist-song element and assign to a playlistSongElements constant.
-
 const highlightCurrentSong = () => {
-    const playlistSongElements = document.querySelectorAll('.playlist-son');
-}
+    const playlistSongElements = document.querySelectorAll(".playlist-song");
+    const songToHighlight = document.getElementById(
+        `song-${userData?.currentSong?.id}`
+    );
+
+
+};
 
 const renderSongs = (array) => {
     const songsHTML = array
