b1192ec28f7c38da63ae2ef8b892cc7fd26545c9
JC <j.c.ashley4363@gmail.com>
2024-02-10 09:17:58 -0600

    musicPlayer completed step 78

--- javaScriptAlgorithmsDataStructures/03musicPlayer/script.js
+++ javaScriptAlgorithmsDataStructures/03musicPlayer/script.js
@@ -156,7 +156,10 @@ const deleteSong = (id) => {
     renderSongs(userData?.songs);
     highlightCurrentSong();
     setPlayButtonAccessibleText();
+    // Step 78
+    // Next, you need to check if the playlist is empty. If it is, you should reset the userData object to its original state.
 
+    // Use an if statement to check if the userData?.songs has a length of 0.
 
 
 };
