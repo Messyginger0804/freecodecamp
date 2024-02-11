9213642f836c98b1979617b65a3566d2daa48c97
JC <j.c.ashley4363@gmail.com>
2024-02-10 21:26:37 -0600

    musicPlayer project completed step 86

--- javaScriptAlgorithmsDataStructures/03musicPlayer/script.js
+++ javaScriptAlgorithmsDataStructures/03musicPlayer/script.js
@@ -250,12 +250,7 @@ previousButton.addEventListener("click", playPreviousSong);
 
 shuffleButton.addEventListener("click", shuffle);
 
-// Step 86
-// All the core functionalities are now in place. The only issue now is that the next song does not automatically play when the currently playing song ends.
-
-// To fix that, you can set up an event listener which will detect when the currently playing song ends. The ended event listener is appropriate for this. It is fired when the playback of a media reaches the end.
-
-// Add an event listener to the audio element which listens for the ended event. Pass in a callback using arrow syntax with empty curly braces.
+audio.addEventListener("ended", () => { });
 
 userData?.songs.sort((a, b) => {
     if (a.title < b.title) {
