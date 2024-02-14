9436a7e76f58f503238b14b1dacfc95186f96018
JC <j.c.ashley4363@gmail.com>
2024-02-14 09:42:50 -0600

    footballTeamCards completed step 10

--- javaScriptAlgorithmsDataStructures/06footballTeamCards/script.js
+++ javaScriptAlgorithmsDataStructures/06footballTeamCards/script.js
@@ -1,3 +1,6 @@
+// Step 10
+// Below the isWorldCupWinner property, add a new key called headCoach with a value of an empty object. Inside that object, add a property with a key of coachName and a string value of Carlos Bilardo. Below that property, add another key called matches with a number value of 7.
+
 const teamName = document.getElementById("team");
 const typeOfSport = document.getElementById("sport");
 const worldCupYear = document.getElementById("year");
@@ -9,4 +12,8 @@ const myFavoriteFootballTeam = {
     sport: "Football",
     year: 1986,
     isWorldCupWinner: true,
+    headCoach: {
+        coachName: 'Carlos Bilardo',
+        matches: 7
+    }
 };
