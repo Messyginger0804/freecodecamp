e1de5688a9066477bd6dcb58de1cf04616cd358f
JC <j.c.ashley4363@gmail.com>
2024-02-14 09:33:41 -0600

    footballTeamCards completed step 4

--- javaScriptAlgorithmsDataStructures/06footballTeamCards/script.js
+++ javaScriptAlgorithmsDataStructures/06footballTeamCards/script.js
@@ -1,6 +1,10 @@
+// Step 4
+// Create one more const variable called playersDropdownList and assign it the id of players using the getElementById method.
+
 const teamName = document.getElementById("team");
 const typeOfSport = document.getElementById("sport");
 const worldCupYear = document.getElementById("year");
 
 const headCoach = document.getElementById('head-coach');
 const playerCards = document.getElementById('player-cards');
+const playersDropdownList = document.getElementById('players');
