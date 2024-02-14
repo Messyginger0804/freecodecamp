212597d5e9c625de115e1f908aa019f27af80af9
JC <j.c.ashley4363@gmail.com>
2024-02-14 09:27:48 -0600

    footballTeamCards completed step 2

--- javaScriptAlgorithmsDataStructures/06footballTeamCards/script.js
+++ javaScriptAlgorithmsDataStructures/06footballTeamCards/script.js
@@ -1,8 +1,6 @@
-// Step 1
-// In this project, you will build a set of football team cards and learn about nested objects, object destructuring, default parameters, event listeners, and switch statements. All of the HTML and CSS for this project has been provided for you.
-
-// Start by accessing the id called team from the HTML document and storing it in a const variable called teamName.
-
-// Remember, you can use the getElementById method for this.
+// Step 2
+// Next, access the id called sport from the HTML document and store it in a const variable called typeOfSport. Below that variable, assign the id of year to a const variable called worldCupYear.
 
 const teamName = document.getElementById('team');
+const typeOfSport = document.getElementById('sport');
+const worldCupYear = document.getElementById('year');
