13a23da35a50b91cf44c552098d865032d031dc6
JC <j.c.ashley4363@gmail.com>
2024-02-14 09:34:42 -0600

    footballTeamCards completed step 5

--- javaScriptAlgorithmsDataStructures/06footballTeamCards/script.js
+++ javaScriptAlgorithmsDataStructures/06footballTeamCards/script.js
@@ -1,5 +1,7 @@
-// Step 4
-// Create one more const variable called playersDropdownList and assign it the id of players using the getElementById method.
+// Step 5
+// Now it is time to build out the data structure that will hold all of the information for your football team.
+
+// Below the variables you just created, create a new const variable called myFavoriteFootballTeam and assign it an empty object.
 
 const teamName = document.getElementById("team");
 const typeOfSport = document.getElementById("sport");
@@ -8,3 +10,5 @@ const worldCupYear = document.getElementById("year");
 const headCoach = document.getElementById('head-coach');
 const playerCards = document.getElementById('player-cards');
 const playersDropdownList = document.getElementById('players');
+
+const myFavoriteFootballTeam = {};
