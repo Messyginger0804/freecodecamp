d7bbae95e58c49e737f8dd93faced8f476eab67f
JC <j.c.ashley4363@gmail.com>
2024-02-14 09:38:32 -0600

    footballTeamCards completed step 8

--- javaScriptAlgorithmsDataStructures/06footballTeamCards/script.js
+++ javaScriptAlgorithmsDataStructures/06footballTeamCards/script.js
@@ -1,15 +1,12 @@
-// Step 6
-// Inside the myFavoriteFootballTeam object, add a new property with a key named team and a string value of Argentina.
-
 const teamName = document.getElementById("team");
 const typeOfSport = document.getElementById("sport");
 const worldCupYear = document.getElementById("year");
-
-const headCoach = document.getElementById('head-coach');
-const playerCards = document.getElementById('player-cards');
-const playersDropdownList = document.getElementById('players');
-
+const headCoach = document.getElementById("head-coach");
+const playerCards = document.getElementById("player-cards");
+const playersDropdownList = document.getElementById("players");
 const myFavoriteFootballTeam = {
-    team: 'Argentina',
-    sport: 'Football'
+    team: "Argentina",
+    sport: "Football",
+    year: 1986,
+
 };
