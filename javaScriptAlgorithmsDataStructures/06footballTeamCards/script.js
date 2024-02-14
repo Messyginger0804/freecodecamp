677d79707f6761a2ee39a6027aa8240f55adae90
JC <j.c.ashley4363@gmail.com>
2024-02-14 09:29:36 -0600

    footballTeamCards completed step 3

--- javaScriptAlgorithmsDataStructures/06footballTeamCards/script.js
+++ javaScriptAlgorithmsDataStructures/06footballTeamCards/script.js
@@ -1,6 +1,6 @@
-// Step 2
-// Next, access the id called sport from the HTML document and store it in a const variable called typeOfSport. Below that variable, assign the id of year to a const variable called worldCupYear.
+const teamName = document.getElementById("team");
+const typeOfSport = document.getElementById("sport");
+const worldCupYear = document.getElementById("year");
 
-const teamName = document.getElementById('team');
-const typeOfSport = document.getElementById('sport');
-const worldCupYear = document.getElementById('year');
+const headCoach = document.getElementById('head-coach');
+const playerCards = document.getElementById('player-cards');
