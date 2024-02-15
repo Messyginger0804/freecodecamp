1b29143f43fa479f795fa94c5a22184bbd9ead63
JC <j.c.ashley4363@gmail.com>
2024-02-14 20:40:04 -0600

    footballTeamCards completed step 17

--- javaScriptAlgorithmsDataStructures/06footballTeamCards/script.js
+++ javaScriptAlgorithmsDataStructures/06footballTeamCards/script.js
@@ -172,10 +172,22 @@ const myFavoriteFootballTeam = {
 };
 
 Object.freeze(myFavoriteFootballTeam);
+// Step 17
+// In the last two steps, you have been accessing properties from the myFavoriteFootballTeam object using dot notation and assigning them to new const variables. But in JavaScript, there is an easier way to accomplish the same goal.
+
+// The object destructuring syntax allows you to unpack values from arrays and objects:
+
+// const developerObj = {
+//   name: "Jessica Wilkins",
+//   isDeveloper: true
+// };
+
+// // Object destructuring
+// const { name, isDeveloper } = developerObj;
+// Rewrite the two lines of code below using the new destructuring syntax. Your answer should be one line of code.
 
 
+const { sport, team } = myFavoriteFootballTeam;
 
-const sport = myFavoriteFootballTeam.sport;
-const team = myFavoriteFootballTeam.team;
 
 
