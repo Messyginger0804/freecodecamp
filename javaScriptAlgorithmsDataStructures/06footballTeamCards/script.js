9f97db185091d330e27fc4ed8987d0674cbc04f1
JC <j.c.ashley4363@gmail.com>
2024-02-14 21:00:33 -0600

    footballTeamCards completed step 24

--- javaScriptAlgorithmsDataStructures/06footballTeamCards/script.js
+++ javaScriptAlgorithmsDataStructures/06footballTeamCards/script.js
@@ -180,12 +180,17 @@ teamName.textContent = team;
 worldCupYear.textContent = year;
 headCoach.textContent = coachName;
 
-const setPlayerCards = () => {
+const setPlayerCards = (arr = players) => {
 
 }
+// Step 24
+// Function parameters can be initialized with default values. If a function is called without an argument, then the default value will be used:
 
-// Step 23
-// Now you will start building out the function that will show player cards based on the selections made by the user in the Filter Teammates dropdown menu.
+// const greeting = (name = "Anonymous") => {
+//   return "Hello " + name;
+// } 
 
-// Start by creating an empty arrow function called setPlayerCards. You do not need to add a parameter because that will be taken care of in the next step.
+// console.log(greeting("John")); // Hello John
+// console.log(greeting()); // Hello Anonymous
+// Add a new parameter to your setPlayerCards function called arr and assign it a default value of players.
 
