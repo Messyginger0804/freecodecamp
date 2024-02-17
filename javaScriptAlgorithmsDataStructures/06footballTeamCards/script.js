db7eb812d89f50a9cdf8e52d09c48ece44780dab
JC <j.c.ashley4363@gmail.com>
2024-02-17 15:17:13 -0600

    footballTeamCards project completed step 38

--- javaScriptAlgorithmsDataStructures/06footballTeamCards/script.js
+++ javaScriptAlgorithmsDataStructures/06footballTeamCards/script.js
@@ -181,32 +181,26 @@ worldCupYear.textContent = year;
 headCoach.textContent = coachName;
 
 const setPlayerCards = (arr = players) => {
-
-
-    playerCards.innerHTML += arr.map(
-        ({ name, position, number, isCaptain, nickname }) => {
-            `
+    playerCards.innerHTML += arr
+        .map(
+            ({ name, position, number, isCaptain, nickname }) =>
+                `
         <div class="player-card">
           <h2>${name} ${isCaptain ? "(Captain)" : ""}</h2>
           <p>Position: ${position}</p>
           <p>Number: ${number}</p>
-          <p>Nickname: ${nickname ? nickname : "N/A"}</p>
+          <p>Nickname: ${nickname !== null ? nickname : "N/A"}</p>
         </div>
-      `;
-        }
-    ).join('');
-
-
+      `
+        )
+        .join("");
 };
 
-playersDropdownList.addEventListener('change', (e) => {
-    console.log(e.target.value);
-})
+playersDropdownList.addEventListener("change", (e) => {
+    playerCards.innerHTML = "";
 
 
-// Step 37
-// Inside the callback function, add a console.log with the value of e.target.value.
 
-// Open the console, and make a selection from the teammates dropdown menu. You should see the value of that selection in the console.
 
-// e.target.value represents the value property from the playersDropdownList element. In future steps, you will use this value to show player cards based on the position they play.
+
+});
