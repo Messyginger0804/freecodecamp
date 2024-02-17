37f9c6b59de76d7e5458513a3486b1003847f822
JC <j.c.ashley4363@gmail.com>
2024-02-17 09:25:11 -0600

    footballTeamCards project completed step 26

--- javaScriptAlgorithmsDataStructures/06footballTeamCards/script.js
+++ javaScriptAlgorithmsDataStructures/06footballTeamCards/script.js
@@ -181,14 +181,11 @@ worldCupYear.textContent = year;
 headCoach.textContent = coachName;
 
 const setPlayerCards = (arr = players) => {
-    arr.map(() => {
-        playerCards.innerHTML
+
+
+    playerCards.innerHTML += arr.map(() => {
+
     })
-}
 
-// Step 25
-// The next step is to create a new array that will be responsible for adding the player card information to the page.
 
-// Inside the setPlayerCards function, start by adding the map method to arr that will take in an empty callback function. Then, use the addition assignment += operator to assign the new array to playerCards.innerHTML.
-
-// Remember that the innerHTML property gets, or in this case, sets the HTML markup for the playerCards element.
+};
