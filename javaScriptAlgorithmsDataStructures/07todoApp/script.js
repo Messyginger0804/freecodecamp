2440149e2bc34022319c3bb60eea5406891c17e4
JC <j.c.ashley4363@gmail.com>
2024-02-17 19:56:45 -0600

    todoApp completed step 5

--- javaScriptAlgorithmsDataStructures/07todoApp/script.js
+++ javaScriptAlgorithmsDataStructures/07todoApp/script.js
@@ -10,5 +10,10 @@ const titleInput = document.getElementById("title-input");
 const dateInput = document.getElementById("date-input");
 const descriptionInput = document.getElementById("description-input");
 
-// Step 4
-// The last set of elements you need to get from the HTML file are the date-input and description-input elements. Save them in the variables dateInput and descriptionInput respectively.
+const taskData = [];
+let currentTask = {};
+
+// Step 5
+// Create a taskData constant and set it to an empty array. This array will store all the tasks along with their associated data, including title, due date, and description. This storage will enable you to keep track of tasks, display them on the page, and save them to localStorage.
+
+// Use let to create a currentTask variable and set it to an empty object. This variable will be used to track the state when editing and discarding tasks.
