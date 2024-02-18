150d4f6ef2ce02e138983c8ce92eacb279ef9f16
JC <j.c.ashley4363@gmail.com>
2024-02-17 20:01:38 -0600

    todoApp completed step 6

--- javaScriptAlgorithmsDataStructures/07todoApp/script.js
+++ javaScriptAlgorithmsDataStructures/07todoApp/script.js
@@ -13,7 +13,7 @@ const descriptionInput = document.getElementById("description-input");
 const taskData = [];
 let currentTask = {};
 
-// Step 5
-// Create a taskData constant and set it to an empty array. This array will store all the tasks along with their associated data, including title, due date, and description. This storage will enable you to keep track of tasks, display them on the page, and save them to localStorage.
+openTaskFormBtn.addEventListener("click", () =>
+    taskForm.classList.toggle("hidden")
+);
 
-// Use let to create a currentTask variable and set it to an empty object. This variable will be used to track the state when editing and discarding tasks.
