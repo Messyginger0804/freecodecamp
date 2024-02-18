a6e45c120f9ffb368af2104435f9fae7c067fc8e
JC <j.c.ashley4363@gmail.com>
2024-02-17 19:50:07 -0600

    todoApp completed step 2

--- javaScriptAlgorithmsDataStructures/07todoApp/script.js
+++ javaScriptAlgorithmsDataStructures/07todoApp/script.js
@@ -1,10 +1,7 @@
-// Step 1
-// In this project, you will learn how localStorage works in JavaScript by building a Todo app. LocalStorage is a web storage feature of JavaScript that lets you persist data by storing the data as a key:value pair.
+const taskForm = document.getElementById("task-form");
+const confirmCloseDialog = document.getElementById("confirm-close-dialog");
+const openTaskFormBtn = document.getElementById("open-task-form-btn");
+const closeTaskFormBtn = document.getElementById("close-task-form-btn");
+const addOrUpdateTaskBtn = document.getElementById("add-or-update-task-btn");
+const cancelBtn = document.getElementById("cancel-btn");
 
-// The HTML and CSS for this project have been provided for you. Take a look at the files to get yourself familiarized with them.
-
-// Begin by accessing the task-form, confirm-close-dialog, and open-task-form-btn elements with the getElementById() method. Save them in the variables taskForm, confirmCloseDialog, and openTaskFormBtn.
-
-const taskForm = document.getElementById('taskForm');
-const confirmCloseDialog = document.getElementById('confirmCloseDialog');
-const openTaskFormBtn = document.getElementById('openTaskForm');
