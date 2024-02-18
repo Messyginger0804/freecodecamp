fd38fc68f91f9a939acce4082ad87eddf1e65d83
JC <j.c.ashley4363@gmail.com>
2024-02-17 19:52:51 -0600

    todoApp completed step 3

--- javaScriptAlgorithmsDataStructures/07todoApp/script.js
+++ javaScriptAlgorithmsDataStructures/07todoApp/script.js
@@ -4,4 +4,9 @@ const openTaskFormBtn = document.getElementById("open-task-form-btn");
 const closeTaskFormBtn = document.getElementById("close-task-form-btn");
 const addOrUpdateTaskBtn = document.getElementById("add-or-update-task-btn");
 const cancelBtn = document.getElementById("cancel-btn");
+const discardBtn = document.getElementById("discard-btn");
+const tasksContainer = document.getElementById("tasks-container");
+const titleInput = document.getElementById("title-input");
 
+// Step 3
+// Next, access the discard-btn, tasks-container, and title-input elements using the getElementById() method. Save them in variables named discardBtn, tasksContainer, and titleInput, respectively.
