391b20d0770146622d6671f7de60d7bdcf7f6857
JC <j.c.ashley4363@gmail.com>
2024-02-17 19:17:41 -0600

    footballTeamCards project completed step 42

--- javaScriptAlgorithmsDataStructures/06footballTeamCards/script.js
+++ javaScriptAlgorithmsDataStructures/06footballTeamCards/script.js
@@ -202,11 +202,11 @@ playersDropdownList.addEventListener("change", (e) => {
     switch (e.target.value) {
         case "nickname":
             setPlayerCards(players.filter(player => player.nickname != null));
+            break;
     }
 
 });
 
-// Step 41
-// Call the setPlayerCards function with an argument of players.filter().
-
-// Inside the filter method, add a callback function with a parameter called player and implicitly return player.nickname is not null.
+// Step 42
+// Before you can move onto the next case, you will need to add a break statement.
+// Below your setPlayerCards call, add a break statement.
