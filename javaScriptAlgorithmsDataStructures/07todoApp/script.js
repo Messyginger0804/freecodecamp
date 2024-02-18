02af59675001c857508f0e6f7686f39a5d877ba9
JC <j.c.ashley4363@gmail.com>
2024-02-17 20:18:21 -0600

    todoApp completed step 7

--- javaScriptAlgorithmsDataStructures/07todoApp/script.js
+++ javaScriptAlgorithmsDataStructures/07todoApp/script.js
@@ -17,3 +17,14 @@ openTaskFormBtn.addEventListener("click", () =>
     taskForm.classList.toggle("hidden")
 );
 
+closeTaskFormBtn.addEventListener("click", () => {
+    confirmCloseDialog.showModal();
+});
+
+// Step 7
+// The HTML dialog element has a showModal() method that can be used to display a modal dialog box on a web page.
+
+// dialogElement.showModal();
+// Add an event listener to the closeTaskFormBtn variable and pass in a click event for the first argument and a callback function for the second argument.
+
+// For the callback function, call the showModal() method on the confirmCloseDialog element. This will display a modal with the Discard and Cancel buttons.
