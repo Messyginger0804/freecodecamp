c70e71b01fbe6b2a747029a1259dde3081cf336d
JC <j.c.ashley4363@gmail.com>
2024-02-17 21:27:17 -0600

    todoApp completed step 11

--- javaScriptAlgorithmsDataStructures/07todoApp/script.js
+++ javaScriptAlgorithmsDataStructures/07todoApp/script.js
@@ -28,11 +28,10 @@ discardBtn.addEventListener("click", () => {
     taskForm.classList.toggle("hidden");
 });
 
+
 taskForm.addEventListener("submit", (e) => {
     e.preventDefault();
-})
 
-// Step 10
-// Now that you've worked on opening and closing the modal, it's time to get the values from the input fields, save them into the taskData array, and display them on the page.
+    const dataArrIndex = taskData.findIndex((item) => item.id === currentTask.id);
 
-// To start, add a submit event listener to your taskForm element and pass in e as the parameter of your arrow function. Inside the curly braces, use the preventDefault() method to stop the browser from refreshing the page after submitting the form.
+});
