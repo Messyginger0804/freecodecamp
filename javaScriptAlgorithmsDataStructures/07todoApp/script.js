8ca86930251295af278854502396bad68dae905e
JC <j.c.ashley4363@gmail.com>
2024-02-19 19:57:06 -0600

    todoApp completed step 14

--- javaScriptAlgorithmsDataStructures/07todoApp/script.js
+++ javaScriptAlgorithmsDataStructures/07todoApp/script.js
@@ -35,12 +35,11 @@ taskForm.addEventListener("submit", (e) => {
     const dataArrIndex = taskData.findIndex((item) => item.id === currentTask.id);
     const taskObj = {
         id: `${titleInput.value.toLowerCase().split(' ').join('-')}-${Date.now()}`,
+        title: titleInput.value,
+        date: dateInput.value,
+        description: descriptionInput.value,
     };
 });
 
-// Step 13
-// To make the id more unique, add another hyphen and use Date.now().
-
-// Date.now() returns the number of milliseconds elapsed since January 1, 1970 00:00:00 UTC.
-
-// console.log(Date.now()); // 1628586800000
+// Step 14
+// Retrieve the values from the titleInput, dateInput, and descriptionInput fields, and then save them in the properties title, date, and description of the taskObj object.
