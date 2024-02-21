dad135e45cd123ad7d9f57d29996167509ae62f5
JC <j.c.ashley4363@gmail.com>
2024-02-20 22:22:00 -0600

    todoApp completed step 21

--- javaScriptAlgorithmsDataStructures/07todoApp/script.js
+++ javaScriptAlgorithmsDataStructures/07todoApp/script.js
@@ -48,6 +48,8 @@ taskForm.addEventListener("submit", (e) => {
         (tasksContainer.innerHTML += `
         <div class="task" id="${id}">
         <p><strong>Title:</strong>${title}</p>
+        <p><strong>Date:</strong>${date}</p>
+        <p><strong>Description:</strong>${description}</p>
         </div>
       `)
     }
