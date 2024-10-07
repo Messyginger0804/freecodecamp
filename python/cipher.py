a093faab611e31a3c0460eb6633478e248bfb38c
Jeremy JC Ashley <113396637+Messyginger0804@users.noreply.github.com>
2024-10-07 20:43:14 +0000

    completed step 40

--- python/cipher.py
+++ python/cipher.py
@@ -17,7 +17,9 @@ text = 'Hello World'
 shift = 3
 alphabet = 'abcdefghijklmnopqrstuvwxyz'
 encrypted_text = ''
+
 for char in text.lower():
+    print(char == ' ')
     index = alphabet.find(char)
     new_index = index + shift
     encrypted_text += alphabet[new_index]
