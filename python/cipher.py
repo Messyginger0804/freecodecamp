1b57d4ce497e70ccfd16f923bc2a04ab2b48392e
Jeremy JC Ashley <113396637+Messyginger0804@users.noreply.github.com>
2024-10-07 21:22:01 +0000

    cipher completed step 49

--- python/cipher.py
+++ python/cipher.py
@@ -1,17 +1,31 @@
 
-# Step 47
-# Next, modify your print() call to print 'encrypted text:', encrypted_text and put it outside the for loop, so that the encrypted string is printed one time.
+# Step 49
+# A function is essentially a reusable block of code. You have already met some built-in functions, like print(), find() and len(). But you can also define custom functions like this:
+
+# Example Code
+# def function_name():
+#     <code>
+# A function declaration starts with the def keyword followed by the function name — a valid variable name — and a pair of parentheses. The declaration ends with a colon.
+
+# Right after your shift variable, declare a function called caesar and indent all the following lines to give your new function a body.
 
 text = 'Hello Zaira'
 shift = 3
-alphabet = 'abcdefghijklmnopqrstuvwxyz'
-encrypted_text = ''
 
-for char in text.lower():
-    if char == ' ':
-        encrypted_text += char
-    else:
-        index = alphabet.find(char)
-        new_index = (index + shift) % len(alphabet)
-        encrypted_text += alphabet[new_index]
-print('encrypted text:', encrypted_text)
+def caesar():
+    alphabet = 'abcdefghijklmnopqrstuvwxyz'
+    encrypted_text = ''
+    
+    for char in text.lower():
+        if char == ' ':
+            encrypted_text += char
+        else:
+            index = alphabet.find(char)
+            new_index = (index + shift) % len(alphabet)
+            encrypted_text += alphabet[new_index]
+    
+    print('plain text:', text)
+    print('encrypted text:', encrypted_text)
+
+# Call the function to run the encryption
+caesar()
