20da10bec5aef1448e568296308effe9543a32ff
Jeremy JC Ashley <113396637+Messyginger0804@users.noreply.github.com>
2024-10-07 19:09:13 +0000

    completed step 26

--- python/cipher.py
+++ python/cipher.py
@@ -1,11 +1,10 @@
 
-# Step 25
-# The iteration variable can have any valid name, but it's convenient to give it a meaningful name.
-
-# Rename your i variable to char.
+# Step 26
+# Inside the for loop, before printing the current character, declare a variable called index and assign the value returned by alphabet.find(char) to this variable.
 
 text = 'Hello World'
 shift = 3
 alphabet = 'abcdefghijklmnopqrstuvwxyz'
 for char in text:
+    index = alphabet.find(char)
     print(char)
