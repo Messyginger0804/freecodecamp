d5b259d8c317981569de788b048a39d28932a710
Jeremy JC Ashley <113396637+Messyginger0804@users.noreply.github.com>
2024-10-07 18:05:47 +0000

    completed step 10

--- python/cipher.py
+++ python/cipher.py
@@ -1,10 +1,8 @@
 
-# Step 9
-# As you can see, the output of printing type(text) is <class 'str'>, which means that your variable is a string, indicated as str.
-
-# Now go to a new line and create another variable called shift and assign the value 3 to this variable.
+# Step 10
+# And now print your new variable.
 
 text = 'Hello World'
 print(type(text))
-
 shift = 3
+print(shift)
