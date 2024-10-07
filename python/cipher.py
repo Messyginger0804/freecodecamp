ab44e9f5b057b12b70e27ec98d6d9a78f1134893
Jeremy JC Ashley <113396637+Messyginger0804@users.noreply.github.com>
2024-10-07 20:12:59 +0000

    set up step 40

--- python/cipher.py
+++ python/cipher.py
@@ -1,12 +1,17 @@
 
-# Step 39
-# You can obtain the same effect of a = a + b by using the addition assignment operator:
+# Step 40
+# Comparison operators allow you to compare two objects based on their values. You can use a comparison operator by placing it between the objects you want to compare. They return a Boolean value — namely True or False — depending on the truthfulness of the expression.
 
-# Example Code
-# a += b
-# The addition assignment operator enables you to add a value to a variable and then assign the result to that variable.
+# Python has the following comparison operators:
 
-# Use the += operator to add a value and assign it at the same time to encrypted_text.
+# Operator	Meaning
+# ==	Equal
+# !=	Not equal
+# >	Greater than
+# <	Less than
+# >=	Greater than or equal to
+# <=	Less than or equal to
+# At the beginning of your loop body, print the result of comparing char with a space (' '). Use the equality operator == for that.
 
 text = 'Hello World'
 shift = 3
