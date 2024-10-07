180a6bc2187c499675eebc414c16e1c9c9ada70f
Jeremy JC Ashley <113396637+Messyginger0804@users.noreply.github.com>
2024-10-07 20:49:05 +0000

    completed step 41

--- python/cipher.py
+++ python/cipher.py
@@ -1,17 +1,13 @@
 
-# Step 40
-# Comparison operators allow you to compare two objects based on their values. You can use a comparison operator by placing it between the objects you want to compare. They return a Boolean value — namely True or False — depending on the truthfulness of the expression.
+# Step 41
+# Currently, spaces get encrypted as 'c'. To maintain the original spacing in the plain message, you'll require a conditional if statement. This is composed of the if keyword, a condition, and a colon :.
 
-# Python has the following comparison operators:
+# Example Code
+# if x != 0:
+#     print(x)
+# In the example above, the condition of the if statement is x != 0. The code print(x), inside the if statement body, runs only when the condition evaluates to True (in this example, meaning that x is different from zero).
 
-# Operator	Meaning
-# ==	Equal
-# !=	Not equal
-# >	Greater than
-# <	Less than
-# >=	Greater than or equal to
-# <=	Less than or equal to
-# At the beginning of your loop body, print the result of comparing char with a space (' '). Use the equality operator == for that.
+# At the top of your for loop, replace print(char == ' ') with an if statement. The condition of this if statement should evaluate to True if char is an empty space and False otherwise. Inside the if body, print the string 'space!'. Remember to indent this line.
 
 text = 'Hello World'
 shift = 3
@@ -19,8 +15,10 @@ alphabet = 'abcdefghijklmnopqrstuvwxyz'
 encrypted_text = ''
 
 for char in text.lower():
-    print(char == ' ')
-    index = alphabet.find(char)
-    new_index = index + shift
-    encrypted_text += alphabet[new_index]
+    if char == ' ':
+        print('space!')
+    else:
+        index = alphabet.find(char)
+        new_index = (index + shift) % len(alphabet)  # Ensure the index wraps around if needed
+        encrypted_text += alphabet[new_index]
     print('char:', char, 'encrypted text:', encrypted_text)
