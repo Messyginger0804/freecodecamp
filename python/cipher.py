c0ce5be50647dda304dcea84d06c958786e8dcf3
Jeremy JC Ashley <113396637+Messyginger0804@users.noreply.github.com>
2024-10-07 18:40:27 +0000

    completed step 20

--- python/cipher.py
+++ python/cipher.py
@@ -1,6 +1,5 @@
-
-# Step 19
-# Declare a new variable named shifted. Use the bracket notation to access the value of alphabet at index index and assign it to your new variable.
+# Step 20
+# Print your shifted variable.
 
 text = 'Hello World'
 shift = 3
