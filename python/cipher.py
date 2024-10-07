371fdaaad25efa4c49236a76520a7758d7d1aff7
Jeremy JC Ashley <113396637+Messyginger0804@users.noreply.github.com>
2024-10-07 19:11:30 +0000

    completed step 27

--- python/cipher.py
+++ python/cipher.py
@@ -1,10 +1,13 @@
 
-# Step 26
-# Inside the for loop, before printing the current character, declare a variable called index and assign the value returned by alphabet.find(char) to this variable.
+# Step 27
+# Currently, the print() function is taking a single argument char, but it can take multiple arguments, separated by a comma.
+
+# Add a second argument to print(char) so that it prints the character and its index inside the alphabet.
 
 text = 'Hello World'
 shift = 3
 alphabet = 'abcdefghijklmnopqrstuvwxyz'
+
 for char in text:
     index = alphabet.find(char)
-    print(char)
+    print(char, index)
