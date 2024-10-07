ee97e1dd6f394826c856cb0b434de127061d1683
Jeremy JC Ashley <113396637+Messyginger0804@users.noreply.github.com>
2024-10-07 18:06:52 +0000

    completed step 11

--- python/cipher.py
+++ python/cipher.py
@@ -1,8 +1,8 @@
 
-# Step 10
-# And now print your new variable.
+# Step 11
+# Modify your print(shift) call to print the data type of your shift variable.
 
 text = 'Hello World'
 print(type(text))
 shift = 3
-print(shift)
+print(type(shift))
