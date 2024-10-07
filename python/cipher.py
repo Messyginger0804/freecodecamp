408efdcf458fd08e1b5cb856619e5866719c8e6f
Jeremy JC Ashley <113396637+Messyginger0804@users.noreply.github.com>
2024-10-07 17:37:45 +0000

    completed step 4

--- python/cipher.py
+++ python/cipher.py
@@ -1,10 +1,13 @@
-# Step 3
-# You can use the built-in function print() to print the output of your code on the terminal.
+# Step 4
+# An argument is an object or an expression passed to a function — added between the opening and closing parentheses — when it is called:
 
-# Functions are reusable code blocks that you can call, or invoke, to run their code when you need them. To call a function, you just need to write a pair of parentheses next to its name. You will learn more about functions very soon.
+# Example Code
+# greet = 'Hello!'
+# print(greet)
+# The code in the example above would print the string 'Hello!', which is the value of the variable greet passed to print() as the argument.
 
-# For now, go to a new line and add an empty call to the print() function. You should not see any output yet.
+# Print your text variable to the screen by passing the text variable as the argument to the print() function.
 
 
 text = 'Hello World'
-print()
+print(text)
