91f783bc78efe66dd569216b1e5606dcc2bf0b9b
Jeremy JC Ashley <113396637+Messyginger0804@users.noreply.github.com>
2024-10-07 18:28:57 +0000

    completed step 18

--- python/cipher.py
+++ python/cipher.py
@@ -1,12 +1,10 @@
-# Step 17
-# .find() returns the index of the matching character inside the string. If the character is not found, it returns -1. As you can see, the first character in text, uppercase 'H', is not found, since alphabet contains only lowercase letters.
 
-# You can transform a string into its lowercase equivalent with the .lower() method. Add another print() call to print text.lower() and see the output.
+# # Step 18
+# Remove the last print() call. Then, instead of text[0], pass text[0].lower() as the argument to your .find() call and see the output.
+
 
 text = 'Hello World'
 shift = 3
 alphabet = 'abcdefghijklmnopqrstuvwxyz'
-index = alphabet.find(text[0])
+index = alphabet.find(text[0].lower())
 print(index)
-
-print(text.lower())
