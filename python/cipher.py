ecff2af8c3cecd83447d74d3821bf50c0fd57439
Jeremy JC Ashley <113396637+Messyginger0804@users.noreply.github.com>
2024-10-08 14:55:46 +0000

    cipher project completed step 51

--- python/cipher.py
+++ python/cipher.py
@@ -16,16 +16,8 @@ def caesar():
     print('plain text:', text)
     print('encrypted text:', encrypted_text)
 
-print(alphabet)
 
 
-# Step 50
-# In Python, the scope of a variable determines where that variable can be accessed:
 
-# Variables defined outside a function have a global scope and they can be accessed from any part of your code.
-
-# Variables defined inside a function are local to that function and cannot be accessed outside of it.
-
-# To see this in action, try to print the alphabet variable at the end of your code. This will raise a NameError exception.
-
-# You should see an error message indicating that alphabet is not defined. This is because alphabet is defined inside the caesar function and is not accessible outside of it.
+# Step 51
+# Now, fix the error by removing the line that tries to print the alphabet variable outside of the caesar function.
