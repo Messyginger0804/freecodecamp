277c923ee0bf69d9cc8f215c9a46bfd302622a5d
Jeremy JC Ashley <113396637+Messyginger0804@users.noreply.github.com>
2024-10-08 21:02:51 +0000

    cipher step 55 completed

--- python/cipher.py
+++ python/cipher.py
@@ -1,17 +1,17 @@
-message = 'Hello Zaira'
-offset = 3
+text = 'Hello Zaira'
+shift = 3
+
 def caesar(message, offset):
     alphabet = 'abcdefghijklmnopqrstuvwxyz'
-    encrypted_message = ''
+    encrypted_text = ''
 
     for char in message.lower():
         if char == ' ':
-            encrypted_message += char
+            encrypted_text += char
         else:
             index = alphabet.find(char)
             new_index = (index + offset) % len(alphabet)
-            encrypted_message += alphabet[new_index]
-    print('plain message:', message)
-    print('encrypted message:', encrypted_message)
-
-caesar()
+            encrypted_text += alphabet[new_index]
+    print('plain text:', message)
+    print('encrypted text:', encrypted_text)
+caesar(text, shift)
