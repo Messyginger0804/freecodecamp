d5c62852e027f34a5a488551423e3147879803ad
Jeremy JC Ashley <113396637+Messyginger0804@users.noreply.github.com>
2024-10-09 15:10:45 +0000

    cipher project completed step 88

--- python/cipher.py
+++ python/cipher.py
@@ -1,5 +1,6 @@
 text = 'mrttaqrhknsw ih puggrur'
 custom_key = 'python'
+
 def vigenere(message, key, direction=1):
     key_index = 0
     alphabet = 'abcdefghijklmnopqrstuvwxyz'
@@ -28,8 +29,6 @@ def encrypt(message, key):
     
 def decrypt(message, key):
     return vigenere(message, key, -1)
-    
-encryption = encrypt(text, custom_key)
-print(encryption)
-decryption = decrypt(encryption, custom_key)
-print(decryption)
+
+# decryption = decrypt(encryption, custom_key)
+# print(decryption)
