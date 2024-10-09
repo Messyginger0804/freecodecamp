15c745bbe409895a87d015645cb1bd288d97a38d
Jeremy JC Ashley <113396637+Messyginger0804@users.noreply.github.com>
2024-10-09 14:28:02 +0000

    cipher project completed step 76

--- python/cipher.py
+++ python/cipher.py
@@ -4,25 +4,26 @@ custom_key = 'python'
 def vigenere(message, key, direction):
     key_index = 0
     alphabet = 'abcdefghijklmnopqrstuvwxyz'
-    final_message= ''
+    final_message = ''
 
     for char in message.lower():
     
         # Append space to the message
         if char == ' ':
-            final_message+= char
+            final_message += char
         else:        
-            # Find the right key character to encode
+            # Find the right key character to encode/decode
             key_char = key[key_index % len(key)]
             key_index += 1
 
-            # Define the offset and the encrypted letter
+            # Define the offset and the encrypted/decrypted letter
             offset = alphabet.index(key_char)
             index = alphabet.find(char)
             new_index = (index + offset*direction) % len(alphabet)
-            final_message+= alphabet[new_index]
+            final_message += alphabet[new_index]
     
     return final_message
+    
 encryption = vigenere(text, custom_key, 1)
 print(encryption)
 decryption = vigenere(encryption, custom_key, -1)
