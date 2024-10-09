2b1dc8098f148af1f9678729a07df07c5487bade
Jeremy JC Ashley <113396637+Messyginger0804@users.noreply.github.com>
2024-10-09 13:50:06 +0000

    cipher project completed step 60

--- python/cipher.py
+++ python/cipher.py
@@ -1,10 +1,14 @@
+
 text = 'Hello Zaira'
 custom_key = 'python'
+
 def vigenere(message, key):
+    key_index = 0
     alphabet = 'abcdefghijklmnopqrstuvwxyz'
     encrypted_text = ''
 
     for char in message.lower():
+        
         if char == ' ':
             encrypted_text += char
         else:
