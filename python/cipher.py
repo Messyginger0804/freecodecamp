09bd058f3b1afb94a1a8af2fa71369b28b23fed1
Jeremy JC Ashley <113396637+Messyginger0804@users.noreply.github.com>
2024-10-09 14:57:36 +0000

    cipher project completed step 84

--- python/cipher.py
+++ python/cipher.py
@@ -23,11 +23,10 @@ def vigenere(message, key, direction=1):
             final_message += alphabet[new_index]
     
     return final_message
+def encrypt(message, key):
+    return vigenere(message, key)
 
 encryption = vigenere(text, custom_key)
 print(encryption)
 decryption = vigenere(encryption, custom_key, -1)
 print(decryption)
-
-def encrypt(message, key):
-    pass
