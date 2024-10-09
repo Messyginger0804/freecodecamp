e0862e9ba12d0db8e0b515664d2f9a127dc844ff
Jeremy JC Ashley <113396637+Messyginger0804@users.noreply.github.com>
2024-10-09 15:07:54 +0000

    cipher project completed step 87

--- python/cipher.py
+++ python/cipher.py
@@ -1,12 +1,12 @@
-text = 'Hello Zaira!'
+text = 'mrttaqrhknsw ih puggrur'
 custom_key = 'python'
-
 def vigenere(message, key, direction=1):
     key_index = 0
     alphabet = 'abcdefghijklmnopqrstuvwxyz'
     final_message = ''
 
     for char in message.lower():
+
         # Append any non-letter character to the message
         if not char.isalpha():
             final_message += char
@@ -18,22 +18,18 @@ def vigenere(message, key, direction=1):
             # Define the offset and the encrypted/decrypted letter
             offset = alphabet.index(key_char)
             index = alphabet.find(char)
-            new_index = (index + offset * direction) % len(alphabet)
+            new_index = (index + offset*direction) % len(alphabet)
             final_message += alphabet[new_index]
     
     return final_message
 
-# Encryption function
 def encrypt(message, key):
     return vigenere(message, key)
-
-# Decryption function
+    
 def decrypt(message, key):
     return vigenere(message, key, -1)
-
-# Call the encrypt and decrypt functions
+    
 encryption = encrypt(text, custom_key)
-print("Encrypted:", encryption)
-
+print(encryption)
 decryption = decrypt(encryption, custom_key)
-print("Decrypted:", decryption)
+print(decryption)
