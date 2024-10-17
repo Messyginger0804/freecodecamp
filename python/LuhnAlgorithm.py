5c9e92f9bb69af1a6fa5edaeaf02ad5b056fb95f
Jeremy JC Ashley <113396637+Messyginger0804@users.noreply.github.com>
2024-10-17 16:27:59 +0000

    LuhnAlgorithm project completed step 33

--- python/LuhnAlgorithm.py
+++ python/LuhnAlgorithm.py
@@ -21,6 +21,10 @@ def main():
     card_translation = str.maketrans({'-': '', ' ': ''})
     translated_card_number = card_number.translate(card_translation)
 
-    verify_card_number(translated_card_number)
+    if verify_card_number(translated_card_number):
+        print('VALID!')
+    else:
+        print('INVALID!')
+
 
 main()
