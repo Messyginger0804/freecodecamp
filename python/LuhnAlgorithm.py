b5ad4b33f19b33954463f9221fc828c78a3435c3
Jeremy JC Ashley <113396637+Messyginger0804@users.noreply.github.com>
2024-10-17 14:52:39 +0000

    LuhnAlgorithm project completed step 19

--- python/LuhnAlgorithm.py
+++ python/LuhnAlgorithm.py
@@ -2,7 +2,10 @@ def verify_card_number(card_number):
     sum_of_odd_digits = 0
     card_number_reversed = card_number[::-1]
     odd_digits = card_number_reversed[::2]
-    print(odd_digits)
+
+    for digit in odd_digits:
+        print(digit)
+    
 def main():
     card_number = '4111-1111-4555-1142'
     card_translation = str.maketrans({'-': '', ' ': ''})
