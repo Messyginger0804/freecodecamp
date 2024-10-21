582203c06477c77243f52257945b3140d5ad03be
Jeremy JC Ashley <113396637+Messyginger0804@users.noreply.github.com>
2024-10-21 16:34:37 +0000

    BiSectionMethod project completed step 10

--- python/BiSectionMethod.py
+++ python/BiSectionMethod.py
@@ -7,11 +7,6 @@ def square_root_bisection(square_target, tolerance=1e-7, max_iterations=100):
     elif square_target == 0:
         root = 0
         print(f'The square root of {square_target} is 0')
-
     else:
         low = 0
         high = max(1, square_target)
-        root = None
-        
-        for _ in range(max_iterations):
-            pass
