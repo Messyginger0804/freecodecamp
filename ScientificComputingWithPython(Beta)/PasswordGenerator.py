a5f30a14cc7229eff45e4394ea4f4d3201293662
Jeremy JC Ashley <113396637+Messyginger0804@users.noreply.github.com>
2024-10-21 18:18:07 +0000

    PasswordGenerator projecct completed step 13

--- ScientificComputingWithPython(Beta)/PasswordGenerator.py
+++ ScientificComputingWithPython(Beta)/PasswordGenerator.py
@@ -1,11 +1,11 @@
 import secrets
 import string
 
+def generate_password():
+    # Define the possible characters for the password
+    letters = string.ascii_letters
+    digits = string.digits
+    symbols = string.punctuation
 
-# Define the possible characters for the password
-letters = string.ascii_letters
-digits = string.digits
-symbols = string.punctuation
-
-# Combine all characters
-all_characters = letters + digits + symbols
+    # Combine all characters
+    all_characters = letters + digits + symbols
