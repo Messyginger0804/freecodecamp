e39d3895c5a7241b452281fd7b487d00e37e3086
Jeremy JC Ashley <113396637+Messyginger0804@users.noreply.github.com>
2024-10-21 16:52:38 +0000

    BiSectionMethod project 100% completed

--- python/BiSectionMethod.py
+++ python/BiSectionMethod.py
@@ -28,10 +28,11 @@ def square_root_bisection(square_target, tolerance=1e-7, max_iterations=100):
 
         if root is None:
             print(f"Failed to converge within {max_iterations} iterations.")
-        
+    
         else:   
             print(f'The square root of {square_target} is approximately {root}')
     
     return root
 
 N = 16
+square_root_bisection(N)
