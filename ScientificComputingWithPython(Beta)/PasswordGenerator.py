454122fb542787e3c9ae3c4fab13bb9a9ea79b0f
Jeremy JC Ashley <113396637+Messyginger0804@users.noreply.github.com>
2024-10-23 19:40:31 +0000

    PasswordGenerator project completed step 24

--- ScientificComputingWithPython(Beta)/PasswordGenerator.py
+++ ScientificComputingWithPython(Beta)/PasswordGenerator.py
@@ -1,6 +1,7 @@
 import secrets
 import string
 
+
 def generate_password(length, nums, special_chars, uppercase, lowercase):
     # Define the possible characters for the password
     letters = string.ascii_letters
@@ -9,13 +10,13 @@ def generate_password(length, nums, special_chars, uppercase, lowercase):
 
     # Combine all characters
     all_characters = letters + digits + symbols
-
-    password = ''
-    # Generate password
-    for _ in range(length):
-        password += secrets.choice(all_characters)
-        
-    return password
+    
+    while True:
+        password = ''
+        # Generate password
+        for _ in range(length):
+            password += secrets.choice(all_characters)
+        return password
 
 # new_password = generate_password(8)
 # print(new_password)
