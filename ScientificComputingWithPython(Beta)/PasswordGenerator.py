eda6590febe317bdf042d3f8900eb034812c1b42
Jeremy JC Ashley <113396637+Messyginger0804@users.noreply.github.com>
2024-11-06 17:30:17 +0000

    passwordGenerator proejct completed step 48

--- ScientificComputingWithPython(Beta)/PasswordGenerator.py
+++ ScientificComputingWithPython(Beta)/PasswordGenerator.py
@@ -17,17 +17,18 @@ def generate_password(length, nums, special_chars, uppercase, lowercase):
         # Generate password
         for _ in range(length):
             password += secrets.choice(all_characters)
+        
         constraints = [
             (nums, r'\d'),
             (lowercase, r'[a-z]'),
             (uppercase, r'[A-Z]'),
-            (special_chars, r'[^a-zA-Z0-9]')
+            (special_chars, r'\W')
         ]        
+
     return password
     
 # new_password = generate_password(8)
 # print(new_password)
-
-pattern = r'\.'
+pattern = r'\W'
 quote = 'Not all those who wander are lost.'
-# print(re.findall(pattern, quote))
+print(re.findall(pattern, quote))
